977a82088ddde431caa817d36bc9192a3d3cc860
Brian Di Palma <offler@gmail.com>
2015-02-20 21:18:16 +0000

    Simple test for module.exports formatting.

---
+++ test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var assert = require('assert');
+
+var mocha = require('mocha');
+var esformatter = require('esformatter');
+
+var moduleExportsPlugin = require('./');
+
+esformatter.register(moduleExportsPlugin);
+
+mocha.describe('module exports plugin', function() {
+	mocha.it('should add an empty line before module.exports.', function() {
+		// Given.
+		var codeStr = 'function MyClass() {}\nmodule.exports = MyClass;';
+
+		// When.
+		var formattedCode = esformatter.format(codeStr);
+
+		// Then.
+		assert.equal(formattedCode, 'function MyClass() {\n}\n\nmodule.exports = MyClass;');
+	});
+});
