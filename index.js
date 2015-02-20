1cd9ab95f00db8e80209a70ab210715556be4d9b
Brian Di Palma <offler@gmail.com>
2015-02-20 21:23:50 +0000

    Formatter that passes simple test for formatting module.exports.

---
+++ index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var isBr = require('rocambole-token').isBr;
+var after = require('rocambole-token').after;
+var before = require('rocambole-token').before;
+
+exports.tokenBefore = function(token) {
+	if (isModuleExportsToken(token)) {
+		var prev = token.prev;
+
+		// If the token before the module exports isn't a newline add one.
+		if (isBr(prev) === false) {
+			before(token, createNewLineToken());
+		}
+
+		var prevToPrev = prev.prev;
+
+		// If the token two steps back is also not a newline add one.
+		if (isBr(prevToPrev) === false) {
+			after(prevToPrev, createNewLineToken());
+		}
+	}
+};
+
+/**
+ * Returns true if the provided token is the module in `module.exports`.
+ * @param   {Object}  token Rocambole token
+ * @returns {Boolean} True if module.export token
+ */
+function isModuleExportsToken(token) {
+	var isModuleIdentifier = token.type === 'Identifier' && token.value === 'module';
+
+	if (isModuleIdentifier && token.next && token.next.next) {
+		var isFollowedByDot = token.next.value === '.' && token.next.type === 'Punctuator';
+
+		if (isFollowedByDot && token.next.next) {
+			var tokenAfterDot = token.next.next;
+
+			return tokenAfterDot.type === 'Identifier' && tokenAfterDot.value === 'exports';
+		}
+	}
+
+	return false;
+}
+
+/**
+ * Creates a newline token
+ * @returns {Object} Newline rocambole token
+ */
+function createNewLineToken() {
+	return {
+		value: '\n',
+		type: 'LineBreak'
+	};
+}
