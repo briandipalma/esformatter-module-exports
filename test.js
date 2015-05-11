19f4fe7b12bbb7ebd43da22d545b2e0c29494f0a
Brian Di Palma <offler@gmail.com>
2015-05-11 08:53:40 +0100

    Add more tests and config files.

--- test.js
+++ test.js
@@ -20,4 +20,15 @@ mocha.describe('module exports plugin', function() {
 		// Then.
 		assert.equal(formattedCode, 'function MyClass() {\n}\n\nmodule.exports = MyClass;');
 	});
+
+	mocha.it('should add an two empty lines before module.exports.', function() {
+		// Given.
+		var codeStr = 'function MyClass() {}module.exports = MyClass;';
+
+		// When.
+		var formattedCode = esformatter.format(codeStr);
+
+		// Then.
+		assert.equal(formattedCode, 'function MyClass() {\n}\n\nmodule.exports = MyClass;');
+	});
 });
